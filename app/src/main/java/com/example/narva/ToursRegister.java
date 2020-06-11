19469fc763ff704713a6e0efe54ee58fb4ddf90d
santeri13 <Samulipoh>
2020-06-11 03:33:14 +0300

    New features

---
+++ app/src/main/java/com/example/narva/ToursRegister.java
@@ -0,0 +1,19 @@
+package com.example.narva;
+
+public class ToursRegister {
+    String tours;
+
+    public String getTours() {
+        return tours;
+    }
+
+    public void setTours(String tours) {
+        this.tours = tours;
+    }
+    public ToursRegister(){
+
+    }
+    public ToursRegister( String tours) {
+        this.tours = tours;
+    }
+}
